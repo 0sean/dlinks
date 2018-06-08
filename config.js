b85c05e3a7d9121f8a53bc07aede510a0ad23845
Ken <34059898+thehydrogen@users.noreply.github.com>
2018-06-08 11:42:21 +0200

    Switch to JS based configuration file

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  /*
+    The web server port. e.x: 80 for http://localhost/
+    or 8080 for http://localhost:8080/
+  */
+  port: 80,
+  
+  /*
+    Title used for pages, footer, pages, etc.
+  */
+  title: "DLinks",
+  
+  /*
+    Autor used in footer.
+  */
+  author: "Anidox",
+  
+  /*
+    Description used for metadata, etc.
+  */
+  description: "A self-hostable Discord URL shortener."
+};
